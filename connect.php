0a01c4b81536ee84697b1dfd047952aa65bfd8b7
buymom <51958595+buymom@users.noreply.github.com>
2021-04-13 01:07:03 +0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,16 @@
 <?php
 //connection to database
-//define ('DB_HOST','localhost');
-//define ('DB_USER','root');
-//define ('DB_PASSWORD','');
-//define ('DB_NAME','blogsdb');
+define ('DB_HOST','localhost');
+define ('DB_USER','root');
+define ('DB_PASSWORD','');
+define ('DB_NAME','blogsdb');
 
 $conn = new mysqli(DB_HOST,DB_USER,DB_PASSWORD);
 
-$DB_HOST = getenv("MYSQL_SERVICE_HOST");
-$DB_PORT = getenv("MYSQL_SERVICE_PORT");
-$DB_USER = getenv("username");
-$DB_PASSWORD = getenv("password");
+//$DB_HOST = getenv("MYSQL_SERVICE_HOST");
+//$DB_PORT = getenv("MYSQL_SERVICE_PORT");
+//$DB_USER = getenv("username");
+//$DB_PASSWORD = getenv("password");
 
 
 
